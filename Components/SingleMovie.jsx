d7d08cf896967e7071864ba18683ef6b511057d4
Nicolebalzano <nicole.balzano03@gmail.com>
2025-07-01 14:18:35 +0200

    feat:aggiunta recensioni implementata nel front end

--- Components/SingleMovie.jsx
+++ Components/SingleMovie.jsx
@@ -3,17 +3,30 @@ import { useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 import ReviewCard from "./ReviewCard";
+import ReviewForm from "../reviews/ReviewForm";
 
 const SingleMovie = () => {
     const navigate = useNavigate();
      const {slug} = useParams();
 const [movie, setMovie] = useState(null)
   
-     useEffect(()=> {
-        axios.get(`http://localhost:3000/movies/${slug}`).then((resp) => {
-            setMovie(resp.data.data)
-        })
-     }, [slug])
+    
+  useEffect(() => {
+    getMovieDetails();
+  }, [slug]);
+
+  const getMovieDetails = () => {
+    axios
+      .get(`http://localhost:3000/movies/${slug}`)
+      .then((resp) => {
+        setMovie(resp.data.data);
+      })
+      .catch((err) => {
+        if (err.status === 404) {
+          navigate("/not-found");
+        }
+      });
+  };
        const goBack = (event) => {
         event.preventDefault();
         navigate(-1);
@@ -46,6 +59,19 @@ return(
                     </div>)
                 })}
             </div>)}
+        </section>
+        <section className="container py-5">
+            <h2 className="text-center">Lascia una recensione</h2>
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-8 col-xl-6">
+                    <div className="card">
+                        <ReviewForm
+                    movie_id={movie.id}
+                    reloadReviews={getMovieDetails}
+                  />
+                    </div>
+                </div>
+            </div>
         </section>
         </>
     )}
